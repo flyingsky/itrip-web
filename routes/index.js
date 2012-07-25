4ce88640037f924402c731da16b25cc16ce34259
Ramon Liu <llmfei@gmail.com>
2012-07-26 00:44:29 +0800

    use services instead routes/api

--- routes/index.js
+++ routes/index.js
@@ -3,36 +3,34 @@ var fs = require('fs');
 /*
  * GET home page.
  */
-var objUtil = require("../lib/object-utils"),
-    gallery = require('./gallery');
+var utils = require('../lib/utils'),
+   objUtil = require("../lib/object-utils"),
+   gallery = require('./gallery');
+
+var apis = {};
 
 module.exports = function (app) {
 
-    app.get('/', gallery.index);
-    app.get('/index', gallery.index);
-    app.get('/api', api);
+   app.get('/', gallery.index);
+   app.get('/index', gallery.index);
+   app.get('/api', api);
 
-    fs.readdirSync(__dirname).filter(isFileToBeAutoLoad).forEach(addRoutes.bind({}, app, ''));
-    fs.readdirSync(__dirname + '/api').forEach(addRoutes.bind({}, app, '/api'));
+   fs.readdirSync(__dirname).filter(isFileToBeAutoLoad).forEach(addRoutes.bind({}, app, ''));
+   fs.readdirSync(process.cwd() + '/services').forEach(addRoutes.bind({findAPI:true}, app, '/api'));
 
 };
 
-function index (req, res){
-  res.render('index', { title: 'iTrip' });
-};
+function index(req, res) {
+   res.render('index', { title:'iTrip' });
+}
+;
 
 /**
  * API Home page listing all apis
  */
-function api (req, res) {
-    //FIXME: make me fleible.
-    var apis = [ { name: "Gallery", url: '/api/gallery/index'},
-                 { name: "GalleryPage2", url: '/gallery/index?page=2'}, 
-                 { name: "Upload", url: '/api/gallery/upload'}, 
-                 { name: "SearchGallery", url: '/api/gallery/search'},
-                 { name: "tripRecord", url: '/api/gallery/triprecord'} ];
-
-    res.render('api', { apis: apis, title: 'apis' });
+function api(req, res) {
+   console.log(apis);
+   res.render('api', { apis:apis, title:'apis' });
 }
 
 
@@ -41,18 +39,27 @@ function api (req, res) {
  */
 var notAutoLoadFiles = ['index.js'];
 
-function isFileToBeAutoLoad (filename) {
+function isFileToBeAutoLoad(filename) {
    return /\.js$/.test(filename) && notAutoLoadFiles.indexOf(filename) === -1;
 }
 
-function addRoutes (app, prefixInput, filename) {
-  var prefix = prefixInput ? prefixInput : '',
+function addRoutes(app, prefixInput, filename) {
+   var self = this,
+      prefix = prefixInput ? prefixInput : '',
       name = filename.substr(0, filename.lastIndexOf('.')),
       exps = require(['.', prefix, name].join('/'));
 
-  // TODO: separate GET/POST/ALL
-  objUtil.forEach(exps, function (key, value) {
-     console.log([prefix, name.toLowerCase(), key.toLowerCase()].join('/'));
-     app.all([prefix, name.toLowerCase(), key.toLowerCase()].join('/'), value);
-  });
+   // TODO: separate GET/POST/ALL
+   objUtil.forEach(exps, function (key, value) {
+      console.log([prefix, name.toLowerCase(), key.toLowerCase()].join('/'));
+      if (!self.findAPI || (self.findAPI && utils.isFunction(value))) {
+         var url = [prefix, name.toLowerCase(), key.toLowerCase()].join('/');
+         app.all(url, value);
+
+         if (self.findAPI) {
+            apis[name] = apis[name] || {};
+            apis[name][key] = url;
+         }
+      }
+   });
 }
